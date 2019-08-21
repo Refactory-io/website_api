f97c9f10fc5601495bfe6215616a2d0406c4c23e
Jakub Skrzypek <jakub.skrzypek12@gmail.com>
2019-08-21 22:12:10 +0200

    Add @GetMapping annotation for ArticleController#getArticles

--- src/main/java/io/refactory/website_api/article/ArticleController.java
+++ src/main/java/io/refactory/website_api/article/ArticleController.java
@@ -1,5 +1,6 @@
 package io.refactory.website_api.article;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -8,6 +9,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/articles")
 class ArticleController {
     
+    @GetMapping
     Mono getArticles() {
         return Mono.empty();
     }
